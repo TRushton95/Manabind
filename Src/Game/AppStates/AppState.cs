ef5d40ec0bd2d837f877488ecb7718e6ca82a6aa
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-06-06 18:09:32 +0100

    Added base app state

---
+++ Src/Game/AppStates/AppState.cs
@@ -0,0 +1,43 @@
+﻿using Manabind.Src.UI;
+using Microsoft.Xna.Framework.Input;
+
+namespace Manabind.Src.Game.AppStates
+{
+    public abstract class AppState
+    {
+        #region Fields
+
+        private MouseState currentMouseState, prevMouseState;
+
+        private UIManager uiManager;
+
+        #endregion
+
+        #region Constructors
+
+        public AppState()
+        {
+            this.uiManager = new UIManager();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Update()
+        {
+            UpdateState();
+            UpdateMouseState();
+        }
+
+        protected abstract void UpdateState();
+
+        private void UpdateMouseState()
+        {
+            this.prevMouseState = this.currentMouseState;
+            this.currentMouseState = Mouse.GetState();
+        }
+
+        #endregion
+    }
+}
