960bca93baab790ccd908bbfda935267355291b5
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-06-16 02:28:41 +0100

    Created app.config for path names

---
+++ Appsettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace Manabind
+{
+    public static class Appsettings
+    {
+        public static string MenuUIFileName { get => ConfigurationManager.AppSettings["MenuUIFileName"]; }
+
+        public static string UIDefinitionPath { get => ConfigurationManager.AppSettings["UIDefinitionPath"]; }
+    }
+}
