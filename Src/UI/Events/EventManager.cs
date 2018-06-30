6da0fb97905d94d40f41dcbc58f699f9dfdae938
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-06-30 19:33:55 +0100

    Re-added event response and event manager

---
+++ Src/UI/Events/EventManager.cs
@@ -0,0 +1,53 @@
+﻿using Manabind.Src.UI.Components.Complex;
+using System.Collections.Generic;
+
+namespace Manabind.Src.UI.Events
+{
+    public static class EventManager
+    {
+        #region Fields
+
+        private static List<BaseComplexComponent> listeners;
+
+        #endregion
+
+        #region Static Constructors
+
+        static EventManager()
+        {
+            listeners = new List<BaseComplexComponent>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static void Subscribe(BaseComplexComponent subscriber)
+        {
+            if (!listeners.Contains(subscriber))
+            {
+                listeners.Add(subscriber);
+            }
+        }
+
+        public static void Unsubscribe(BaseComplexComponent unsubscriber)
+        {
+            if (listeners.Contains(unsubscriber))
+            {
+                listeners.Remove(unsubscriber);
+            }
+        }
+
+        public static void ClearSubscribers()
+        {
+            listeners.Clear();
+        }
+
+        public static void PushEvent(UIEvent e)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Src/UI/Events/EventResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Manabind.Src.UI.Events
+{
+    public class EventResponse
+    {
+        #region Constructors
+
+        public EventResponse(string action, UIEvent uiEvent)
+        {
+            this.Action = action;
+            this.UIEvent = uiEvent;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Action { get; }
+
+        public UIEvent UIEvent { get; }
+
+        #endregion
+    }
+}
