02e2d59a266672498a9bc899b4e6cf3e913b8da5
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-07-02 23:08:34 +0100

    Added ImageGraphics class

---
+++ Src/UI/Components/Complex/Icon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Manabind.Src.UI.Components.Basic;
+using Manabind.Src.UI.PositionProfiles;
+using Manabind.Src.UI.Components.BaseInstanceResources;
+using Manabind.Src.UI.Factories;
+
+namespace Manabind.Src.UI.Components.Complex
+{
+    public class Icon : BaseComplexComponent
+    {
+        #region Fields
+
+        private ImageGraphics image;
+
+        #endregion
+
+        #region Constructors
+
+        public Icon()
+            : base()
+        {
+        }
+
+        public Icon(int width, int height, BasePositionProfile positionProfile, int priority, Texture2D defaultTexture, Texture2D hoverTexture)
+            : base(width, height, positionProfile, priority)
+        {
+            this.DefaultTexture = defaultTexture;
+            this.HoverTexture = hoverTexture;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Texture2D DefaultTexture
+        {
+            get;
+            set;
+        }
+
+        public Texture2D HoverTexture
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            this.image.Draw(spriteBatch);
+        }
+
+        public override void Initialise(Rectangle parent)
+        {
+            this.InitialiseCoordinates(parent);
+
+            this.image = new ImageGraphics(this.DefaultTexture, this.HoverTexture, PositionProfileFactory.BuildCenteredRelative());
+        }
+
+        protected override void ClickDetail()
+        {
+        }
+
+        protected override void HoverDetail()
+        {
+            this.image.Hover();
+        }
+
+        protected override void HoverLeaveDetail()
+        {
+            this.image.HoverLeave();
+        }
+
+        #endregion
+    }
+}
