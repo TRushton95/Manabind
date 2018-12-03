21516c22398b4a60daf8d8bf5e45242a600e0b7c
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-12-03 22:55:34 +0000

    Added experimental ListItem type class

---
+++ Src/UI/Components/Complex/ListItems/TextboxListItem.cs
@@ -0,0 +1,37 @@
+﻿namespace Manabind.Src.UI.Components.Complex.ListItems
+{
+    public class TextboxListItem : Textbox
+    {
+        #region Constructors
+
+        public TextboxListItem()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+        protected override void ExecuteEventResponse(string action, object content)
+        {
+            base.ExecuteEventResponse(action, content);
+
+            //TO-DO Dodgy way to extend the switch statement?
+            switch (action)
+            {
+                case "scroll-selection":
+                    this.Text = (string)content;
+                    this.Refresh();
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
