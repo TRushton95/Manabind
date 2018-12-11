604916e634c5dab7577d8d49a949698fec2f8b4b
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-12-11 21:46:52 +0000

    Added properties to Unit

--- Src/Gameplay/Unit.cs
+++ Src/Gameplay/Unit.cs
@@ -1,9 +1,17 @@
-﻿using Manabind.Src.Gameplay.Entities;
+﻿using Manabind.Src.Gameplay.Abilities;
+using Manabind.Src.Gameplay.Entities;
+using System.Collections.Generic;
 
 namespace Manabind.Src.Gameplay
 {
     public class Unit : BaseEntity
     {
+        #region Fields
+
+        public static int Diameter = 25;
+
+        #endregion
+        
         #region Constructors
 
         public Unit()
@@ -14,14 +22,64 @@ namespace Manabind.Src.Gameplay
         #endregion
 
         #region Properties
+        
+        public int MaxHealth
+        {
+            get;
+            set;
+        }
 
+        public int CurrentHealth
+        {
+            get;
+            set;
+        }
 
+        public int MaxEnergy
+        {
+            get;
+            set;
+        }
+
+        public int CurrentEnergy
+        {
+            get;
+            set;
+        }
+
+        public int Team
+        {
+            get;
+            set;
+        }
+
+        public List<Ability> Abilities
+        {
+            get;
+            set;
+        }
+
+        /*
+        public List<BaseTick> Ticks
+        {
+            get;
+            set;
+        }
+        */
 
         #endregion
 
         #region Methods
 
-
+        /*
+        public void CastAbility(Tile targetTile, int abilitySlot)
+        {
+            if (Abilities[abilitySlot] != null)
+            {
+                Abilities[abilitySlot].Execute(targetTile);
+            }
+        }
+        */
 
         #endregion
     }
