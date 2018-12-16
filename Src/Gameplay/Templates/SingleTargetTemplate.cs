c3bb18e947243352bf45ee9bd65db6fd459fa729
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-12-16 02:51:21 +0000

    Added incomplete ITemplate implementations

---
+++ Src/Gameplay/Templates/SingleTargetTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Manabind.Src.Gameplay.Entities.Tiles;
+
+namespace Manabind.Src.Gameplay.Templates
+{
+    public class SingleTargetTemplate : ITemplate
+    {
+        public List<BaseTile> GetAffectedTiles(BaseTile targetTile)
+        {
+            return new List<BaseTile>() { targetTile };
+        }
+    }
+}
