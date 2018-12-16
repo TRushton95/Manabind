0b0b966a7a4e62446793b442782b6b2d570f6941
Tom Rushton <tom.rushton@hotmail.co.uk>
2018-12-16 01:13:41 +0000

    Added effect ticks

---
+++ Src/Gameplay/Abilities/Ticks/BaseTick.cs
@@ -0,0 +1,36 @@
+﻿namespace Manabind.Src.Gameplay.Abilities.Ticks
+{
+    public abstract class BaseTick
+    {
+        #region Constructors
+
+        public BaseTick(int duration)
+        {
+            this.Duration = duration;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Duration
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Tick()
+        {
+            TickResolution();
+            Duration--;
+        }
+
+        public abstract void TickResolution();
+
+        #endregion
+    }
+}
---
+++ Src/Gameplay/Abilities/Ticks/HealTick.cs
@@ -0,0 +1,43 @@
+﻿using Manabind.Src.Gameplay.Abilities.BaseEffects;
+
+namespace Manabind.Src.Gameplay.Abilities.Ticks
+{
+    public class HealTick : BaseTick
+    {
+        #region Constructors
+
+        public HealTick(int value, int duration, Unit target)
+            : base(duration)
+        {
+            this.Value = value;
+            this.Target = target;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Value
+        {
+            get;
+            set;
+        }
+
+        public Unit Target
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void TickResolution()
+        {
+            AtomicEffects.Heal(this.Target, this.Value);
+        }
+
+        #endregion
+    }
+}
